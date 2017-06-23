323ad4ab5264946c46f6a6853cf24362e6e96e54
Antonio Arnaiz Lago <antonio.arnaizlago@gmail.com>
2017-06-23 14:11:11 +0200

    Actualizacion Nuevo Teclado

--- Nuevos_Modulos/Teclado/Sw/Formant_V2_Teclado/Formant_Teclado.h
+++ Nuevos_Modulos/Teclado/Sw/Formant_V2_Teclado/Formant_Teclado.h
@@ -26,19 +26,20 @@
 //
 // ---------------------------------------------------------
 
-#define APP_MODO_DEBUG                   1   // MODO DEBUG: Descomentar este define
-                                            // MODO REAL:  Comentar este define
-                                             
-                                             
+#define APP_MODO_DEBUG     1   // MODO DEBUG: Descomentar este define
+                               // MODO REAL:  Comentar este define
 
-#define IDE_HW_PIN_D0    0
-#define IDE_HW_PIN_D1    1
-#define IDE_HW_PIN_D2    2
-#define IDE_HW_PIN_D3    3
-#define IDE_HW_PIN_D4    4
-#define IDE_HW_PIN_D5    5
+#define IDE_HW_PIN_D0      0   // Pines para generar el codigo de control 
+#define IDE_HW_PIN_D1      1   // del multiplexor, el que selecciona la tecla pulsada
+#define IDE_HW_PIN_D2      2
+#define IDE_HW_PIN_D3      3
+#define IDE_HW_PIN_D4      4
+#define IDE_HW_PIN_D5      5
 
-#define IDE_HW_PIN_GATE  6
+#define IDE_HW_PIN_GATE    6    // Pin para generar la señal GATE
+
+#define IDE_CANAL_ASIGNADO 1    // Canal por el que se comunica el MIDI
+
 
 
 
